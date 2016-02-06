3ee93c1ae8ed9dcc8cc1a4b5fd3123e630d90313
Dmitry Brant <dbrant@wikimedia.org>
2016-02-05 22:40:20 -0500

    Update license.

--- ImageFormats/PcxReader.cs
+++ ImageFormats/PcxReader.cs
@@ -10,60 +10,21 @@ depths, etc).  At the very least, it decodes all PCX images that
 I've found in the wild.  If you find one that it fails to decode,
 let me know!
 
-Copyright 2013 by Dmitry Brant.
-You may use this source code in your application(s) free of charge,
-as long as attribution is given to me (Dmitry Brant) and my URL
-(http://dmitrybrant.com) in your application's "about" box and/or
-documentation. Of course, donations are always welcome:
-http://dmitrybrant.com/donate
+Copyright 2013-2016 Dmitry Brant
+http://dmitrybrant.com
 
-If you would like to use this source code without attribution, please
-contact me through http://dmitrybrant.com, or visit this page:
-http://dmitrybrant.com/noattributionlicense
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
------------------------------------------------------------
-Full License Agreement for this source code module:
+   http://www.apache.org/licenses/LICENSE-2.0
 
-"Author" herein shall refer to Dmitry Brant. "Software" shall refer
-to this source code module.
-This software is supplied to you by the Author in consideration of
-your agreement to the following terms, and your use, installation,
-modification or redistribution of this software constitutes acceptance
-of these terms. If you do not agree with these terms, please do not use,
-install, modify or redistribute this software.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-In consideration of your agreement to abide by the following terms,
-and subject to these terms, the Author grants you a personal,
-non-exclusive license, to use, reproduce, modify and redistribute
-the software, with or without modifications, in source and/or binary
-forms; provided that if you redistribute the software in its entirety
-and without modifications, you must retain this notice and the following
-text and disclaimers in all such redistributions of the software, and
-that in all cases attribution of the Author as the original author
-of the source code shall be included in all such resulting software
-products or distributions. Neither the name, trademarks, service marks
-or logos of the Author may be used to endorse or promote products
-derived from the software without specific prior written permission
-from the Author. Except as expressly stated in this notice, no other
-rights or licenses, express or implied, are granted by the Author
-herein, including but not limited to any patent rights that may be
-infringed by your derivative works or by other works in which the 
-oftware may be incorporated.
-
-The software is provided by the Author on an "AS IS" basis. THE AUTHOR
-MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT
-LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY
-AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE OR ITS USE
-AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT,
-INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
-REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE SOFTWARE, HOWEVER
-CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-STRICT LIABILITY OR OTHERWISE, EVEN IF THE AUTHOR HAS BEEN ADVISED
-OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 namespace DmitryBrant.ImageFormats
