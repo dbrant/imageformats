5cee008018b28bfd118cdceb30201f5320539c00
Dmitry Brant <dbrant@wikimedia.org>
2019-01-22 22:59:43 -0500

    Update copyright.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageViewer")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Brant 2013")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Brant, 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
