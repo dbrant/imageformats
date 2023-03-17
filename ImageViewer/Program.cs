b8353212fd45c04820732da0919c7ffd759add10
Dmitry Brant <me@dmitrybrant.com>
2023-03-17 08:14:18 -0400

    Add high dpi awareness.

--- ImageViewer/Program.cs
+++ ImageViewer/Program.cs
@@ -15,6 +15,7 @@ namespace ImageViewer
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
