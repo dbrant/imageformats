dba0bc2240dad38ce21562314a57f00fbd04e528
Dmitry Brant <me@dmitrybrant.com>
2023-05-19 12:50:23 -0400

    Correctly respect maximum pixel value.

--- ImageFormats/PnmReader.cs
+++ ImageFormats/PnmReader.cs
@@ -179,7 +179,7 @@ namespace DmitryBrant.ImageFormats
                     {
                         for (int i = 0; i < numPixels; i++)
                         {
-                            pixel = bytes[bytePtr++];
+                            pixel = (byte)(bytes[bytePtr++] * 255 / bmpMaxVal);
                             bmpData[elementCount++] = pixel;
                             bmpData[elementCount++] = pixel;
                             bmpData[elementCount++] = pixel;
@@ -190,8 +190,8 @@ namespace DmitryBrant.ImageFormats
                     {
                         for (int i = 0; i < numPixels; i++)
                         {
-                            pixel = bytes[bytePtr++];
-                            bytePtr++;
+                            pixel = (byte)(((bytes[bytePtr] << 8) + bytes[bytePtr + 1]) * 255 / bmpMaxVal);
+                            bytePtr += 2;
                             bmpData[elementCount++] = pixel;
                             bmpData[elementCount++] = pixel;
                             bmpData[elementCount++] = pixel;
@@ -206,9 +206,9 @@ namespace DmitryBrant.ImageFormats
                     {
                         for (int i = 0; i < numPixels; i++)
                         {
-                            bmpData[elementCount++] = bytes[bytePtr + 2];
-                            bmpData[elementCount++] = bytes[bytePtr + 1];
-                            bmpData[elementCount++] = bytes[bytePtr];
+                            bmpData[elementCount++] = (byte)(bytes[bytePtr + 2] * 255 / bmpMaxVal);
+                            bmpData[elementCount++] = (byte)(bytes[bytePtr + 1] * 255 / bmpMaxVal);
+                            bmpData[elementCount++] = (byte)(bytes[bytePtr] * 255 / bmpMaxVal);
                             bytePtr += 3;
                             elementCount++;
                         }
@@ -217,9 +217,9 @@ namespace DmitryBrant.ImageFormats
                     {
                         for (int i = 0; i < numPixels; i++)
                         {
-                            bmpData[elementCount++] = bytes[bytePtr + 4];
-                            bmpData[elementCount++] = bytes[bytePtr + 2];
-                            bmpData[elementCount++] = bytes[bytePtr];
+                            bmpData[elementCount++] = (byte)(((bytes[bytePtr + 4] << 8) + bytes[bytePtr + 5]) * 255 / bmpMaxVal);
+                            bmpData[elementCount++] = (byte)(((bytes[bytePtr + 2] << 8) + bytes[bytePtr + 3]) * 255 / bmpMaxVal);
+                            bmpData[elementCount++] = (byte)(((bytes[bytePtr] << 8) + bytes[bytePtr + 1]) * 255 / bmpMaxVal);
                             bytePtr += 6;
                             elementCount++;
                         }
