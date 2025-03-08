e0b7d67027138c23a7bf3ab78b191b360b6d709b
Dmitry Brant <me@dmitrybrant.com>
2025-03-08 14:56:39 -0500

    Update publish script.

--- publish.bat
+++ publish.bat
@@ -1,1 +1,5 @@
+
+rem If we want to publish a completely self-contained (and enormous) package:
+rem dotnet publish -p:PublishSingleFile=true -r win-x64 -f net8.0-windows -c Release --self-contained true ImageViewer\ImageViewer.csproj -o .\dist
+
 dotnet publish -p:PublishSingleFile=true -r win-x64 -f net8.0-windows -c Release --self-contained false ImageViewer\ImageViewer.csproj -o .\dist
