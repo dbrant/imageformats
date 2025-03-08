0b140cca578458b4b08e045a70c6d9279a615e44
Dmitry Brant <me@dmitrybrant.com>
2025-03-08 14:52:14 -0500

    Update publish script.

--- publish.bat
+++ publish.bat
@@ -1,1 +1,1 @@
-dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained false ImageViewer\ImageViewer.csproj
+dotnet publish -p:PublishSingleFile=true -r win-x64 -f net8.0-windows -c Release --self-contained false ImageViewer\ImageViewer.csproj -o .\dist
